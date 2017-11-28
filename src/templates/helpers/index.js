c583b7ff752722c55724ffd1fd252439fe56bcda
Joe Bartlett <joe@redopop.com>
2017-11-27 22:49:49 -0600

    Add Handlebars helpers to support watermark; #11

--- src/templates/helpers/index.js
+++ src/templates/helpers/index.js
@@ -1,3 +1,5 @@
+/*globals Buffer */
+
 exports.eq = function (v1, v2) {
   return v1 === v2;
 };
@@ -5,3 +7,11 @@ exports.eq = function (v1, v2) {
 exports.or = function (v1, v2) {
   return v1 || v2;
 };
+
+exports.base64 = function (svg) {
+  return Buffer.from(svg).toString('base64');
+}
+
+exports.concat = function () {
+  return Array.prototype.slice.call(arguments, 0, -1).join('');
+}
